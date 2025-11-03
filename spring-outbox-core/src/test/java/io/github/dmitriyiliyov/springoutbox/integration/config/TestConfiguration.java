a3ba22a203ff09d1ed8d4b4821d8213069aad9c7
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2025-11-03 22:47:05 +0100

    fix(integration tests env)

--- spring-outbox-core/src/test/java/io/github/dmitriyiliyov/springoutbox/integration/config/TestConfiguration.java
+++ spring-outbox-core/src/test/java/io/github/dmitriyiliyov/springoutbox/integration/config/TestConfiguration.java
@@ -1,12 +1,22 @@
 package io.github.dmitriyiliyov.springoutbox.integration.config;
 
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @org.springframework.boot.test.context.TestConfiguration
 public class TestConfiguration {
@@ -14,7 +24,7 @@ public class TestConfiguration {
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder.create()
-                .url("jdbc:postgresql://localhost:5432/outbox_test")
+                .url("jdbc:postgresql://localhost:1428/outbox_test")
                 .username("admin")
                 .password("root")
                 .driverClassName("org.postgresql.Driver")
@@ -29,5 +39,23 @@ public class TestConfiguration {
     @Bean
     public MeterRegistry meterRegistry() {
         return new SimpleMeterRegistry();
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, 200);
+        ProducerFactory<String, Object> producerFactory = new DefaultKafkaProducerFactory<>(properties);
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public TransactionTemplate transactionTemplate() {
+        return new TransactionTemplate(new DataSourceTransactionManager(dataSource()));
     }
 }
