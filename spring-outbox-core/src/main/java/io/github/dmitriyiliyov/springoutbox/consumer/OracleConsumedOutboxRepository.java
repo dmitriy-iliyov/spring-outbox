de1f64516e8c6dce6a9d95cdcf37432afd4253c8
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2025-11-27 04:41:58 +0100

    add(idempotent batch insert for oracle)

--- spring-outbox-core/src/main/java/io/github/dmitriyiliyov/springoutbox/consumer/OracleConsumedOutboxRepository.java
+++ spring-outbox-core/src/main/java/io/github/dmitriyiliyov/springoutbox/consumer/OracleConsumedOutboxRepository.java
@@ -1,25 +1,24 @@
 package io.github.dmitriyiliyov.springoutbox.consumer;
 
-import io.github.dmitriyiliyov.springoutbox.publisher.utils.BytesSqlResultSetMapper;
+import io.github.dmitriyiliyov.springoutbox.utils.BytesSqlIdHelper;
+import io.github.dmitriyiliyov.springoutbox.utils.BytesSqlResultSetMapper;
 import io.github.dmitriyiliyov.springoutbox.utils.RepositoryUtils;
-import io.github.dmitriyiliyov.springoutbox.utils.SqlIdHelper;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class OracleConsumedOutboxRepository implements ConsumedOutboxRepository {
 
     protected final JdbcTemplate jdbcTemplate;
-    protected final SqlIdHelper idHelper;
+    protected final BytesSqlIdHelper idHelper;
     protected final BytesSqlResultSetMapper mapper;
 
-    public OracleConsumedOutboxRepository(JdbcTemplate jdbcTemplate, SqlIdHelper idHelper, BytesSqlResultSetMapper mapper) {
+    public OracleConsumedOutboxRepository(JdbcTemplate jdbcTemplate, BytesSqlIdHelper idHelper, BytesSqlResultSetMapper mapper) {
         this.jdbcTemplate = jdbcTemplate;
         this.idHelper = idHelper;
         this.mapper = mapper;
@@ -39,6 +38,50 @@ public class OracleConsumedOutboxRepository implements ConsumedOutboxRepository 
             });
         } catch (DuplicateKeyException e) {
             return 0;
+        }
+    }
+
+    @Transactional
+    @Override
+    public Set<UUID> saveIfAbsent(Set<UUID> ids) {
+        if (!RepositoryUtils.isIdsValid(ids)) {
+            return Collections.emptySet();
+        }
+        String existsIdsSql = """
+            SELECT id 
+            FROM outbox_consumed_events
+            WHERE id IN (%s)
+        """.formatted(RepositoryUtils.generateIdsPlaceholders(ids));
+        List<UUID> existsIds = jdbcTemplate.query(
+                existsIdsSql,
+                ps -> idHelper.setIdsToPs(ps, 1, ids),
+                (rs, rowNum) -> mapper.fromBytesToUuid(rs.getBytes("id"))
+        );
+        Set<UUID> nonExistsIds = ids.stream()
+                .filter(id -> !existsIds.contains(id))
+                .collect(Collectors.toSet());
+        if (!RepositoryUtils.isIdsValid(nonExistsIds)) {
+            return Collections.emptySet();
+        }
+        String insertSql = """
+            INSERT INTO outbox_consumed_events(id, consumed_at)
+            VALUES %s
+        """.formatted(RepositoryUtils.generateValuesPlaceholders(nonExistsIds, 2));
+        Instant consumedAt = Instant.now();
+        try {
+            jdbcTemplate.update(
+                    insertSql,
+                    ps -> {
+                        int paramId = 1;
+                        for (UUID id: nonExistsIds) {
+                            ps.setBytes(paramId++, idHelper.uuidToBytes(id));
+                            ps.setTimestamp(paramId++, Timestamp.from(consumedAt));
+                        }
+                    }
+            );
+            return new HashSet<>(nonExistsIds);
+        } catch (DuplicateKeyException e) {
+            throw new ConcurrentInsertException(nonExistsIds.size(), 0, nonExistsIds);
         }
     }
 
