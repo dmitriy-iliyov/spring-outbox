e2f983cedc0800dab41ddf2fcd5d325030722792
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2026-02-03 15:19:21 +0100

    fix(git ci flow & poms)

--- spring-outbox-core/src/test/java/io/github/dmitriyiliyov/springoutbox/core/publisher/TestOutboxEvent.java
+++
@@ -1,5 +0,0 @@
-package io.github.dmitriyiliyov.springoutbox.core.publisher;
-
-import java.util.UUID;
-
-public record TestOutboxEvent(UUID id){ }
--- spring-outbox-starter/src/test/java/io/github/dmitriyiliyov/springoutbox/starter/DatabaseTypeUnitTests.java
+++ spring-outbox-starter/src/test/java/io/github/dmitriyiliyov/springoutbox/starter/DatabaseTypeUnitTests.java
@@ -3,8 +3,8 @@ package io.github.dmitriyiliyov.springoutbox.starter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DatabaseTypeUnitTests {
 
