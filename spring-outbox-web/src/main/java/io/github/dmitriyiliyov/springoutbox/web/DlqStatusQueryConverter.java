98ae9f9d5e9381ec0bc0fae1c771fabffefbaf6d
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2026-02-17 14:05:34 +0100

    fix(add nullable & opt imports)

--- spring-outbox-web/src/main/java/io/github/dmitriyiliyov/springoutbox/web/DlqStatusQueryConverter.java
+++ spring-outbox-web/src/main/java/io/github/dmitriyiliyov/springoutbox/web/DlqStatusQueryConverter.java
@@ -1,13 +1,14 @@
 package io.github.dmitriyiliyov.springoutbox.web;
 
 import io.github.dmitriyiliyov.springoutbox.core.publisher.dlq.DlqStatus;
+import jakarta.annotation.Nullable;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DlqStatusQueryConverter implements Converter<String, DlqStatus> {
     @Override
-    public DlqStatus convert(String source) {
+    public DlqStatus convert(@Nullable String source) {
         return DlqStatus.fromString(source);
     }
 }
