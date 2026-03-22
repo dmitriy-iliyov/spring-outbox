b475bf598f7b019e09389b803701498b2d34aea9
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2026-03-22 05:42:45 +0100

    add(integration tests)

---
+++ spring-outbox-web/src/test/java/io/github/dmitriyiliyov/springoutbox/web/WebTestApplication.java
@@ -0,0 +1,12 @@
+package io.github.dmitriyiliyov.springoutbox.web;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class
+})
+public class WebTestApplication { }
