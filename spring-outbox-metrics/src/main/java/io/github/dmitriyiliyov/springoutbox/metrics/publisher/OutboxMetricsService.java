e07047b5c31d65126ff03f370f6b95a4c16bbb4a
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2026-03-22 13:50:39 +0100

    fix(javadoc, removed unnecessary)

--- spring-outbox-metrics/src/main/java/io/github/dmitriyiliyov/springoutbox/metrics/publisher/OutboxMetricsService.java
+++ spring-outbox-metrics/src/main/java/io/github/dmitriyiliyov/springoutbox/metrics/publisher/OutboxMetricsService.java
@@ -9,25 +9,20 @@ public interface OutboxMetricsService {
 
     /**
      * Counts the total number of outbox events.
-     *
-     * @return The total count of events.
      */
     long count();
 
     /**
      * Counts the number of outbox events with a specific status.
-     *
-     * @param status The status of the events to count.
-     * @return       The count of events with the given status.
      */
     long countByStatus(EventStatus status);
 
     /**
      * Counts the number of outbox events with a specific event type and status.
      *
-     * @param eventType The type of the events to count.
-     * @param status    The status of the events to count.
-     * @return          The count of events matching the criteria.
+     * @param eventType type of the events to count.
+     * @param status    status of the events to count.
+     * @return          the count of events matching the criteria.
      */
     long countByEventTypeAndStatus(String eventType, EventStatus status);
 }
--- spring-outbox-metrics/src/main/java/io/github/dmitriyiliyov/springoutbox/metrics/publisher/dlq/OutboxDlqMetricsService.java
+++ spring-outbox-metrics/src/main/java/io/github/dmitriyiliyov/springoutbox/metrics/publisher/dlq/OutboxDlqMetricsService.java
@@ -9,25 +9,20 @@ public interface OutboxDlqMetricsService {
 
     /**
      * Counts the total number of DLQ events.
-     *
-     * @return The total count of DLQ events.
      */
     long count();
 
     /**
      * Counts the number of DLQ events with a specific status.
-     *
-     * @param status The status of the DLQ events to count.
-     * @return       The count of DLQ events with the given status.
      */
     long countByStatus(DlqStatus status);
 
     /**
      * Counts the number of DLQ events with a specific event type and status.
      *
-     * @param eventType The type of the DLQ events to count.
-     * @param status    The status of the DLQ events to count.
-     * @return          The count of DLQ events matching the criteria.
+     * @param eventType type of the DLQ events to count.
+     * @param status    status of the DLQ events to count.
+     * @return          the count of DLQ events matching the criteria.
      */
     long countByEventTypeAndStatus(String eventType, DlqStatus status);
 }
